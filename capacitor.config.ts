f466715d55eb4640def2d73e91ef3371d6d1134d
J0nathanMB <belmirojonathan2@gmail.com>
2024-11-12 12:57:01 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Enchentes-e-Logistica',
+  webDir: 'www'
+};
+
+export default config;
